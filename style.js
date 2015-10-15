dbdd1b232d6fbee17fff1c38677c7ee78530f07d
Juan Arbelaez <djxan2014@gmail.com>
2015-10-14 22:52:32 -0400

    game entry

---
+++ style.js
@@ -0,0 +1,67 @@
+var yesNo = prompt ("Ready to enter the haunted house!?");
+
+	if ( yesNo === "yes") {
+		console.log("proceed with caution! MUHAHAHHAHAHAHAH!!");
+	}
+	else { 
+		console.log("Too BAD MUHAHAHAHAHAHAHAH");
+	}
+
+	console.log("you enter the haunted house when suddenly all the lights go off and you find yourself attempting to take your phone out for the flash when suddenly the lights start flickering and theres a clown with a chainsaw running after you what do you do pick up phone or run?");
+
+		
+var userAnswer = prompt ("phone or run?");
+
+	if (userAnswer === "run") {
+    console.log("you run into the abyss when suddenly you stumble on a injured little girl what do you wana do help or RUN");
+
+   						var userAnswer = prompt ("help or run"); 
+			    
+			    
+			    		 		if (userAnswer === "help") {
+			    				console.log("you start helping the girl when suddenly you are surrounded by the undead what do you do stay and fight protect the girl or run and save yourself!? ");
+			   								
+			   								var userAnswer = prompt ("'fight' or 'run'"); 
+			   								            if (userAnswer === "fight") {
+			   								            	console.log("you fought bravely but in the end died!");
+			   								            }
+			   								            else {
+			   								            	console.log("you keep running and finally find hope cause you see a light ,you approach it  and then is gone YOU ARE ALONE");
+			   								            }
+
+
+							    	}
+							    else {
+							    		console.log("you keep running when suddenly you fall into an abyss");
+								
+							    			}
+							    		
+		}	    			
+
+    else {
+    console.log("clown gets close and starts gutting you with the chainsaw  what do you do");
+    var userAnswer = prompt ("'scream' 'crawl' 'cry'");
+     		console.log ("clown shoves his chainsaw in your mouth for being a little jit");
+
+   }
+
+
+
+
+   
+     
+    
+    
+
+
+
+
+
+
+	
+
+    	
+
+
+
+
